11aa0c42b9bf2a925d35c4349e4c4b8b41659130
Twin J <junohanyere@gmail.com>
2023-05-31 13:40:29 +0100

    modified

--- os/shell/lish/shell/shell.c
+++ os/shell/lish/shell/shell.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 #include "shell.h"
-#include "split.h"
+#include "buffer.h"
 
 /**
  * shell - a function to display the command prompt
@@ -19,40 +19,34 @@ int shell(void)
 	char *arg_vector[BUFFER_SIZE];
 	int i, arg_counter;
 	ssize_t flag;
-	size_t *buffer_size;
 
-	/* allocate enough memory for the pointer variables */
-	buffer = malloc(sizeof(char) * BUFFER_SIZE);
-	buffer_size = malloc(sizeof(size_t) * 1024);
+	buffer = malloc(sizeof(char));
 
 	/* loop through to keep displaying the command prompt */
 	while (TRUE)
 	{
-		printf ("|--(user@webhost)-[~]\n");
-		printf ("|-$ ");
+		printf("|--(user@localhost)-[~]\n");
+		printf("|-$ ");
 
-		flag = getline(buffer, buffer_size, stdin);
-
-		if (flag == -1)
-		{
-			exit(EXIT_SUCCESS);
-		}
-
+		getbuffer(buffer);
 		/* save the generated tokens into the args variable */
-		arg_counter = split_buffer(buffer, arg_vector);
+		arg_counter = splitbuffer(arg_vector, buffer);
 
-		/** fucntion accepting args as a paramenter, to handle commands **/
-		printf("Debugging: %s", arg_vector[4]);
-		reset_to_null(arg_vector);
+		/** function accepting args as a paramenter, to handle commands **/
+		/* ......................... */
+
+		/* set the value of arg_vector to null */
+		setnull(arg_vector);
+
 		printf("\n\n");
 
 	}
 
 	/* freeing malloc'ed variables */
 	free(buffer);
-	free(arg_vector);	/* this was malloc'ed in the split_buffer fucntion */
 
-	return (i);	/* return the number of arguments entered. i.e number of words */
+	/* return the number of arguments entered. i.e number of words */
+	return (arg_counter);
 }
 
 int main(void)
