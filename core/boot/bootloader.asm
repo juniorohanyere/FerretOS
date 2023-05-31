d8abdee123cb57a06d708847ffc3c9a47499c1be
Twin J <junohanyere@gmail.com>
2023-05-31 22:39:24 +0100

    new

---
+++ core/boot/bootloader.asm
@@ -0,0 +1,29 @@
+; simple bootloader program
+
+; variable declarations
+section .data
+	string db "Line OS", 0xa
+
+; code wise
+section .text
+	global _start
+
+; entry point
+_start:
+	; print "Line OS" to standard output
+	mov eax, 4	; system call for write
+	mov ebx, 1	; file descriptor for stdout
+	mov ecx, string	; address of string to print
+	mov edx, 13	; number of bytes to print
+	int 0x80	; call kernel (interrupt)
+
+	;exit program with status code 0
+	mov eax, 1	; system call for exit
+	;xor ebx, ebx	; status code 0
+	int 0x80	; call kernel (interrupt)
+
+	jmp _start	; jump to _start, creating an infinite loop
+
+	; magic number
+	times 510 - ($-$$) db 0
+	dw 0xaa55
