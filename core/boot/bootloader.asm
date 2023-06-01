903e9770abb02abb7312103ceef0ff6cd8dab329
Twin J <junohanyere@gmail.com>
2023-06-01 09:21:30 +0100

    modified

--- core/boot/bootloader.asm
+++ core/boot/bootloader.asm
@@ -3,26 +3,28 @@
 [bits 16]	; use 16 bits
 [org 0x7c00]	; start address
 
-; code wise
+; code section
 section .text
 	global _start
 
 ; entry point
 _start:
 	mov ah, 0x0e	; tty mode
-	mov si, string	;
+	mov si, string
 printstring:
-	lodsb
-	cmp al, 0
-	je done
-	int 0x10
+	lodsb	; load the @string into al
+	cmp al, 0	; compare
+	je done	;if al equals 0, jump to done
+	int 0x10	; interrupt
 	jmp printstring; jump to printstring, creating an infinite loop
 done:
-	hlt
+	hlt	; halt the system
 
 string:
-	db "Line OS", 0
+	db "Line OS", 0	; the string to print
 
 ; magic number
 times 510 - ($-$$) db 0
+
+; boot signature
 dw 0xaa55
