5748569ea27c6fce45ac927308bbd143faede1f0
Twin J <junohanyere@gmail.com>
2023-06-11 16:40:43 +0100

    Hot fix

--- core/boot/bootsector.asm
+++ core/boot/bootsector.asm
@@ -2,25 +2,48 @@
 ;;;;		boot sector		;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-[bits 16]	; using 16-bits mode
+[bits 16]
 
-; code section
+; code segment
 section .text
 
-	%include "bootsector.inc"
+	global _start
 
 ;;
  ; _start - entry point
 ;;
 
 _start:
-	jmp _clear	; label from bootloader.asm
+	jmp _clear	; first clear the screen
+;;
+ ; _clear - calls the clear label in screen.asm
+;;
+
+_clear:
+	call clear	; label from screen.asm
+
+;;
+ ; start - entry point after clearing the screen
+;;
+
+start:
+	mov ah, 0x0e	; tty mode, print characters
+
+	mov si, string	; source the @string variable, see data.asm
+
+	call prints	; prints the string to stdout
+
+	call printn	; prints a new line
+
+	call getc	; gets key press
+
+	jmp start	; creates an infinite loop
 
 ; subroutines
-%include "bootloader.asm"
-%include "bootloader.inc"
+%include "bootsector.inc"
 
-; padding: fill remaining spaces with 0(510 zeros minus size of previous codes)
+; padding: fill remaining spaces with 0
+; 510 zeros minus size of previous codes
 times 510 - ($-$$) db 0
 
 ; magic number
--- core/boot/lib.asm
+++ core/boot/lib.asm
@@ -1,4 +1,4 @@
-;section .text
+section .text
 
 ;;
  ; halt - halts or stops the system
