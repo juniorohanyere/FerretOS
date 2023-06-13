5680c11a3f11b43f9c13dcfbf097272b001140ac
Twin J <junohanyere@gmail.com>
2023-06-13 21:31:55 +0100

    modified

--- core/boot/bootloader.asm
+++ core/boot/bootloader.asm
@@ -13,13 +13,29 @@ _clear:
 
 start:
 	mov bx, string
-
 	call prints	; prints the string to stdout
-
 	call printnl	; prints a new line
+
+	call printnl
+
+	mov bx, cont
+	call prints
 
 	call getc	; gets key press
 
-	jmp start	; creates an infinite loop
+	call clear
+
+	mov bx, string
+	call prints
+	call printnl
+
+	call printnl
+
+	call options
+
+	xor bx, bx
+	call gets
+
+	jmp $
 
 %include "bootloader.inc"
---
+++ core/boot/options.asm
@@ -0,0 +1,10 @@
+options:
+	mov bx, opt1
+	call prints
+	call printnl
+
+	mov bx, opt2
+	call prints
+	call printnl
+
+	ret
