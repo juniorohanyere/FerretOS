dee28f613df27098384f6f251478e2da884fffc2
Twin J <junohanyere@gmail.com>
2023-06-19 13:09:35 +0100

    oops! TIMES value is negative (-107)

--- core/boot/bootloader.asm
+++ core/boot/bootloader.asm
@@ -21,16 +21,20 @@ start:
 	call continue	; print the string prompting user to continue
 
 	call getc	; gets key press
-	call clear
 
+	call clear
 	call header
 	call printnl
 
 	call options	; print the bootloader menus
 
-	xor bx, bx	; set bx to index counter
-	call gets	; get buffer
+	mov cx, 4	; set buffer limit to 4 characters
+	mov si, buffer	; store buffer into the si register
 
-	jmp $	; hang
+	call gets	; get buffer and store it in si (buffer)
+
+	call validate	; validate input
+
+	jmp $	; hang here
 
 %include "bootloader.inc"
