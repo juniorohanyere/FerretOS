e82a8626c3d5dfbeb86ee5c2558824e9d81b154d
Twin J <junohanyere@gmail.com>
2023-07-09 11:18:44 +0100

    modifying the bootloader

--- core/boot/stage2/bootsector.asm
+++ core/boot/stage2/bootsector.asm
@@ -1,26 +1,12 @@
 section .text
 	global _start
 
-[bits 32]
+[bits 16]
 _start:
-	mov ax, DATA_SEG
-	mov ds, ax
-	mov ss, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
+	jmp start
 
-	mov ebp, 0x9000
-	mov esp, ebp
-
-	call kernel
-
-[bits 32]
-kernel:
-	[extern main] ; Define calling point. Must have same name as kernel.c 'main' function
-	call main ; Calls the C function. The linker will know where it is placed in memory
 	jmp $
 
-%include "../stage1/gdt.asm"
+; subroutine
 
-times 256 dw 0xdada
+%include "bootsector.inc"
